dc6a7eab3cec730a033500109b282d08060e4ef9
mrkiffie <mrkiffie@gmail.com>
2016-05-21 23:03:50 +0200

    refactor to be ES5 compatible

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 'use strict';
 
-const unit = {
-  dot: 100,
-  dash: 300,
-  space: 30,
-  letterSpace: 200
+var unit = {
+  dot: 150,
+  dash: 450,
+  space: 45,
+  letterSpace: 300
 };
 
-const colors = [
-  '#2196f3',
-  '#4caf50',
-  '#ffeb3b',
-  '#ff9800',
-  '#9c27b0'
-];
+/**
+ * Returns a random hex color
+ * @return {string} A hex color
+ */
+function getRandomColor() {
+  var colors = [
+    '#2196f3',
+    '#4caf50',
+    '#ffeb3b',
+    '#ff9800',
+    '#9c27b0'
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
 
 /**
  * Creates a ripple effect on the element to which it is attached
@@ -21,30 +28,26 @@ const colors = [
  */
 function rippleEffect(event) {
   event.preventDefault();
-
   var target = event.currentTarget;
   var rect = target.getBoundingClientRect();
   var xCenter = event.clientX - rect.left;
   var yCenter = event.clientY - rect.top;
   var radius = Math.min(rect.width, rect.height) / 2;
-
   var ripple = document.createElement('div');
   ripple.className = 'ripple-effect';
-
   ripple.style.width = radius * 2 + 'px';
   ripple.style.height = radius * 2 + 'px';
   ripple.style.top = yCenter - radius + 'px';
   ripple.style.left = xCenter - radius + 'px';
   ripple.style.pointerEvents = 'none';
-  ripple.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  ripple.style.backgroundColor = getRandomColor();
   target.appendChild(ripple);
-
-  window.setTimeout(() => {
+  window.setTimeout(function() {
     ripple.parentNode.removeChild(ripple);
   }, 1000);
 }
 
-class MorseCodePulse {
+var MorseCodePulse = {
   /**
    * @param {string} letter A string representation of the morse code for a
    * given letter
@@ -52,10 +55,10 @@ class MorseCodePulse {
    * @return {array} pulses An array containing the values for pulses for the
    * dots and dashes separated by a spacer
    */
-  static generatePulseArray(letter) {
-    const pulses = [];
+  generatePulseArray: function(letter) {
+    var pulses = [];
     letter.split('')
-      .forEach(pulse => {
+      .forEach(function(pulse) {
         pulses.push(pulse === '-' ? unit.dash : unit.dot);
         pulses.push(unit.space);
       });
@@ -63,88 +66,92 @@ class MorseCodePulse {
     pulses.pop();
     return pulses;
   }
+};
+
+/**
+ * Checkbox constuctor
+ * @param {string} id The id of the checkbox to bind the settings to
+ * @param {function} handler A callback that fires when the checkbox changes,
+ * includes instatiation
+ */
+function Checkbox(id, handler) {
+  this.id = id;
+  this.cbx = document.getElementById(id);
+  this.handler = handler || function() { };
+  this._setting = undefined;
+  this.restoreSetting();
+  this.attachEventListener();
+  this.handler();
 }
 
-class Checkbox {
-  constructor(id, handler) {
-    this.id = id;
-    this.cbx = document.getElementById(id);
-    this.handler = handler || function() { };
-    this._setting = undefined;
-    this.restoreSetting();
-    this.attachEventListener();
-    this.handler();
+Checkbox.prototype.restoreSetting = function() {
+  var value = localStorage.getItem(this.id);
+  if (value === null) {
+    this.setSetting(this.cbx.checked);
+  } else {
+    this.setSetting(value === '1');
   }
 
-  restoreSetting() {
-    const value = localStorage.getItem(this.id);
-    if (value === null) {
-      this.setting = this.cbx.checked;
-    } else {
-      this.setting = value === '1';
-    }
-
-    if (this.setting !== this.cbx.checked) {
-      this.cbx.checked = this.setting;
-    }
+  if (this.getSetting() !== this.cbx.checked) {
+    this.cbx.checked = this.getSetting();
   }
+};
 
-  set setting(value) {
-    this._setting = value;
-    localStorage.setItem(this.id, value ? '1' : '0');
-  }
+Checkbox.prototype.setSetting = function(value) {
+  this._setting = value;
+  localStorage.setItem(this.id, value ? '1' : '0');
+};
 
-  get setting() {
-    return this._setting;
-  }
+Checkbox.prototype.getSetting = function() {
+  return this._setting;
+};
 
-  attachEventListener() {
-    this.cbx.addEventListener('change', this.updateSetting.bind(this), false);
-    this.cbx.addEventListener('change', this.handler.bind(this), false);
-  }
+Checkbox.prototype.attachEventListener = function() {
+  this.cbx.addEventListener('change', this.updateSetting.bind(this), false);
+  this.cbx.addEventListener('change', this.handler.bind(this), false);
+};
 
-  updateSetting(evt) {
-    this.setting = evt.target.checked;
-  }
+Checkbox.prototype.updateSetting = function(evt) {
+  this.setSetting(evt.target.checked);
+};
+
+/**
+ * @param {element} targetElement The DOM Element which the app will be appended
+ */
+function MorseCodeApp(targetElement) {
+  this.targetElement = targetElement;
+  this.attachEventListeners();
+  this.displayPanel = document.getElementById('code');
+  this.vibrate = new Checkbox('vibrate');
 }
 
-class MorseCodeApp {
-  /**
-   * @param {element} targetElement The DOM Element which the app will be appended
-   */
-  constructor(targetElement) {
-    this.targetElement = targetElement;
-    this.attachEventListeners();
-    this.displayPanel = document.getElementById('code');
-    this.vibrate = new Checkbox('vibrate');
+MorseCodeApp.prototype.attachEventListeners = function() {
+  var buttons = [].slice.apply(this.targetElement.querySelectorAll('button'));
+  var len = buttons.length;
+  var i;
+  for (i = 0; i < len; i++) {
+    buttons[i].addEventListener('click', this.eventHandler.bind(this), false);
+    buttons[i].addEventListener('click', rippleEffect, false);
   }
+};
 
-  attachEventListeners() {
-    const buttons = Array.from(this.targetElement.querySelectorAll('button'));
-    buttons.forEach(el => {
-      el.addEventListener('click', this.eventHandler.bind(this));
-      el.addEventListener('click', rippleEffect);
-    });
+MorseCodeApp.prototype.eventHandler = function(evt) {
+  evt.preventDefault();
+  var target = evt.target;
+  var pulses = MorseCodePulse.generatePulseArray(target.value);
+
+  this.displayPanel.innerText = target.value;
+
+  if (this.vibrate.getSetting() === true) {
+    window.navigator.vibrate(pulses);
   }
-
-  eventHandler(evt) {
-    evt.preventDefault();
-    const target = evt.target;
-    const pulses = MorseCodePulse.generatePulseArray(target.value);
-
-    this.displayPanel.innerText = target.value;
-
-    if (this.vibrate.setting === true) {
-      window.navigator.vibrate(pulses);
-    }
-  }
-}
+};
 
 window.morseCodeApp = new MorseCodeApp(document.querySelector('#buttons'));
 
 window.displayCode = new Checkbox('displayCode', function() {
-  const displayPanel = document.getElementById('code');
-  if (this.setting === true) {
+  var displayPanel = document.getElementById('code');
+  if (this.getSetting() === true) {
     displayPanel.style.visibility = 'visible';
   } else {
     displayPanel.style.visibility = 'hidden';
